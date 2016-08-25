9ac2987f8030c5fba8f41ae1ecdb5f29bbf84a3d
Marc Simonton <marcasimonton@gmail.com>
2016-08-25 13:31:34 -0600

    going

---
+++ app.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express')
+let app = express()
+const port = process.env.PORT;
+
+
+
+app.get('/', (req, res) =>  {
+  console.log(process.env);
+  res.send('hello')
+
+})
+
+app.listen(port, () =>  {
+  console.log("Server running on port "+port)
+})
